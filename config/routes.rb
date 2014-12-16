707554088cb00162f8749e08eaf0dd2148a053ee
sophie <sophie.allaoua@hotmail.fr>
2014-12-16 16:21:00 +0000

    moved registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :songs
-
-  resources :comments
-
+  
   root to: "home#index"
+  resources :songs
+  resources :comments
   devise_for :users, :controllers => { registrations: 'registrations' }
 
 end
